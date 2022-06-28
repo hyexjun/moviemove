836188d3fb2a02fab9b51ec42ed6bd2f600fc194
hyebyjun <hyebyjun@gmail.com>
2022-06-28 13:59:54 +0900

    detail 내용 추가, 스타일링

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import styles from './Detail.module.css';
 
 function Detail() {
+  const [loading, setLoading] = useState(true);
   const [details, setDetails] = useState([]);
   const { id } = useParams();
   // console.log(id);
@@ -10,16 +12,29 @@ function Detail() {
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     setDetails(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return (
-    <div>
-      <img src={details.large_cover_image} alt={details.title} />
+  return loading ? (
+    <div>Loading...</div>
+  ) : (
+    <div className={styles.wrap}>
+      <img className={styles.movie__img} src={details.large_cover_image} alt={details.title} />
       <h1>{details.title_long}</h1>
       <div>
-        <h3>genres</h3>
+        <div>
+          <p>Runtime : {details.runtime}mins</p>
+          <p>Rating : {Number(details.rating).toFixed(1)}</p>
+        </div>
+        <ul className={styles.movie__genres}>
+          Genres :
+          {details.genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+        <p className={styles.movie__desc}>{details.description_full}</p>
       </div>
     </div>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,7 +8,7 @@ function Home() {
   const getMovies = async () => {
     const json = await (
       await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=7.0&sort_by=year`
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
       )
     ).json();
     setMovies(json.data.movies);
@@ -30,7 +30,7 @@ function Home() {
               key={movie.id}
               id={movie.id}
               coverImg={movie.medium_cover_image}
-              title={movie.title}
+              title={movie.title_english}
               year={movie.year}
               rating={movie.rating}
               genres={movie.genres}
